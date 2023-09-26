97a05bd1f73408b8d27c4befe564708b025b037d
Kalid Mawi <kalidjamie21@gmail.com>
2023-09-26 19:40:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://stick-and-do-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "removed for security purposes"
 }
 
 // app initialization and database sourcing
